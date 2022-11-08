40009df414ac7b8ef24122651a210fb50856c374
yuanxiaolong <yuanxiaolong@live.com>
2022-11-08 14:19:24 +0800

    自定义线程池中执行线程的统一名称

---
+++ src/main/java/com/yuan/utils/NamedThreadFactory.java
@@ -0,0 +1,32 @@
+package com.yuan.utils;
+
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @author: yuanxiaolong
+ * @Title: NamedThreadFactory
+ * @ProjectName: product_two
+ * @Description: 自定义线程池中执行线程的统一名称
+ * @date: 2022/11/8 14:14
+ */
+public class NamedThreadFactory implements ThreadFactory {
+    private static final AtomicInteger poolNumber = new AtomicInteger(1);
+    private final ThreadGroup group;
+    private final AtomicInteger threadNumber = new AtomicInteger(1);
+    private final String namePrefix;
+
+    public NamedThreadFactory(String name) {
+        SecurityManager s = System.getSecurityManager();
+        group = (s != null) ? s.getThreadGroup() : Thread.currentThread().getThreadGroup();
+        namePrefix = name + "-pool-" + poolNumber.getAndIncrement() + "-thread-";
+    }
+
+    @Override
+    public Thread newThread(Runnable r) {
+        Thread t = new Thread(group, r, namePrefix + threadNumber.getAndIncrement(), 0);
+        if (t.isDaemon()) t.setDaemon(false);
+        if (t.getPriority() != Thread.NORM_PRIORITY) t.setPriority(Thread.NORM_PRIORITY);
+        return t;
+    }
+}
